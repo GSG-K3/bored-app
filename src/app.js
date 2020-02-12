f1bb4a08acc751b5b0d4926642f28d989fe4bae9
bananhaj <bananohaj7@gmail.com>
2020-02-12 09:53:46 +0200

    post request

--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const path = require('path')
 const request = require('request')
+const bodyParser = require('body-parser')
 
 const app =express();
 
+app.set('port',3000)
+
 app.use(express.static(path.join(__dirname,'..','public')))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/tenor',(req,res) => {
+    res.sendFile(path.join(__dirname,'..','public','index.html'))
+
+})
+app.post('/tenor',(req,res) => {
+    res.send(req.body.activity)
+    console.log("hi")
+    res.redirect('index.html')
+})
 
 app.get('/tenor',(req,res,next) => {
     request.get(`http://www.boredapi.com/api/activity?type=music`,(error,response,body)=> {
@@ -19,4 +35,6 @@ app.get('/tenor',(req,res,next) => {
 })
 
 
-app.listen(3000)
+app.listen(app.get('port'), ()=> {
+    console.log('app is running', app.get('port'))
+})
